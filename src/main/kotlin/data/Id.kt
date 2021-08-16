22c49e471819d7dfa5f02a132181b00513ee0025
Kamil Bąk <kamilbak113@gmail.com>
2021-08-16 12:17:36 +0200

    Modified Id class for easier use

--- src/main/kotlin/data/Id.kt
+++ src/main/kotlin/data/Id.kt
@@ -1,17 +1,43 @@
 package data
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 
-@Serializable
-@JvmInline
-value class Id(val value: Int)
+object IdSerializer: KSerializer<Id>
+{
+	override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Id", PrimitiveKind.INT)
+
+	override fun deserialize(decoder: Decoder) = Id(decoder.decodeInt())
+
+	override fun serialize(encoder: Encoder, value: Id)
+	{
+		encoder.encodeInt(value.toInt())
+	}
+}
+
+
+@Suppress("EqualsOrHashCode")
+@Serializable(with = IdSerializer::class)
+class Id(val value: Int)
 {
 	init
 	{
-		require(value > 0){"data.Id in phoenix miner cannot be lower than 1"}
+		require(value > 0) { "Id in phoenix miner cannot be lower than 1" }
 	}
 
 	fun toInt() = value
 
 	override fun toString() = value.toString()
+
+	override fun equals(other: Any?) = when (other)
+	{
+		is Id  -> value == other.value
+		is Int -> value == other
+		else   -> false
+	}
 }
