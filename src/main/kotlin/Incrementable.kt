73e6dc39a9721800421bd53bf42741f1c2a88667
Kamil Bąk <kamilbak113@gmail.com>
2021-09-02 19:11:37 +0200

    Fix broken Shares constructor

--- src/main/kotlin/Incrementable.kt
+++ src/main/kotlin/Incrementable.kt
@@ -2,9 +2,9 @@ import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 // Class which value cannot be lowered, if new value is lower than previous that means error occured and value isn't taking pre-error value into consideration
-class Incrementable<T> : ReadWriteProperty<T, Int>
+class Incrementable<T>(value: Int) : ReadWriteProperty<T, Int>
 {
-	private var current = 0
+	private var current = value
 	private var old = 0
 	override fun getValue(thisRef: T, property: KProperty<*>) = current + old
 
--- src/main/kotlin/miner/Shares.kt
+++ src/main/kotlin/miner/Shares.kt
@@ -4,9 +4,9 @@ import Incrementable
 
 class Shares(valid: Int, stale: Int, rejected: Int)
 {
-	var valid by Incrementable()
-	var stale by Incrementable()
-	var rejected by Incrementable()
+	var valid by Incrementable(valid)
+	var stale by Incrementable(stale)
+	var rejected by Incrementable(rejected)
 	override fun toString() = "$valid/$stale/$rejected"
 	constructor(shares: Array<Int>): this(shares[0], shares[1], shares[2])
 	//constructor(fromString: String): this(fromString.split("/").map { it.toInt() }.toTypedArray())
