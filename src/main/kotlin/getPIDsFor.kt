944c1e0a54e4840f31b8370e00dd2cd770c2407e
Kamil Bąk <kamilbak113@gmail.com>
2021-09-02 18:19:39 +0200

    Fix "No Instance(s) Available." message when no PIDs were found

--- src/main/kotlin/getPIDsFor.kt
+++ src/main/kotlin/getPIDsFor.kt
@@ -6,5 +6,5 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 suspend fun getPIDsFor(imageName: String) = process(
 	// imageName must be wrapped like this because of how ProcessBuilder works https://stackoverflow.com/questions/12124935/processbuilder-adds-extra-quotes-to-command-line#answer-20009602
 	"wmic", "process", "where", "name=\\\"$imageName\\\"", "get", "ProcessID",
-	stdout = Redirect.CAPTURE,
+	stdout = Redirect.CAPTURE, stderr = Redirect.SILENT
 ).output.filter { !it.contains("ProcessId") && it.isNotEmpty() }.map { it.trim().toInt() }
