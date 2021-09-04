1c019376e0d168e820cdceabc6a111d8e1e74d0c
Kamil Bąk <kamilbak113@gmail.com>
2021-09-04 10:33:24 +0200

    Add arg handlers for coroutines

--- src/main/kotlin/functions/argHandlers.kt
+++ src/main/kotlin/functions/argHandlers.kt
@@ -1,5 +1,16 @@
 package functions
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
 fun Array<String>.ifArg(arg: String, block: () -> Unit) = if (arg in this) block() else Unit
 
+fun Array<String>.ifArgCoroutine(arg: String, coroutineScope: CoroutineScope, block: suspend CoroutineScope.() -> Unit) {
+	if (arg in this) coroutineScope.launch(block = block)
+}
+
 fun Array<String>.ifNoArg(arg: String, block: () -> Unit) = if (arg !in this) block() else Unit
+
+fun Array<String>.ifNoArgCoroutine(arg: String, coroutineScope: CoroutineScope, block: suspend CoroutineScope.() -> Unit) {
+	if (arg !in this) coroutineScope.launch(block = block)
+}
