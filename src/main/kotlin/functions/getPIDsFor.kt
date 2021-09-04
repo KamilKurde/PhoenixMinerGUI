fb1cdfc9b5324ba10e8c58e4bc07dc98550866a2
Kamil Bąk <kamilbak113@gmail.com>
2021-09-04 10:36:01 +0200

    fix PID getting not working for names not containing spaces

--- src/main/kotlin/functions/getPIDsFor.kt
+++ src/main/kotlin/functions/getPIDsFor.kt
@@ -5,8 +5,10 @@ import com.github.pgreze.process.process
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoroutinesApi
-suspend fun getPIDsFor(imageName: String) = process(
+suspend fun getPIDsFor(imageName: String): List<Int>
+{
 	// imageName must be wrapped like this because of how ProcessBuilder works https://stackoverflow.com/questions/12124935/processbuilder-adds-extra-quotes-to-command-line#answer-20009602
-	"wmic", "process", "where", "name=\\\"$imageName\\\"", "get", "ProcessID",
-	stdout = Redirect.CAPTURE, stderr = Redirect.SILENT
-).output.filter { !it.contains("ProcessId") && it.isNotEmpty() }.map { it.trim().toInt() }
+	val nameQuery = if(imageName.contains(" ")) "name=\\\"$imageName\\\"" else "name=\"$imageName\""
+	val process = process("wmic", "process", "where", nameQuery , "get", "ProcessID", stdout = Redirect.CAPTURE, stderr = Redirect.SILENT)
+	return process.output.filterNot { it.contains("ProcessId") || it.isEmpty() }.map { it.trim().toInt() }
+}
