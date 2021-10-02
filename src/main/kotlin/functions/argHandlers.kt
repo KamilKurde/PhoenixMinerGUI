0e4d4d48f29b05d5eb0ff80955c3d1ceece19ea9
Kamil Bąk <kamilbak113@gmail.com>
2021-10-02 18:48:07 +0200

    Remove unused functions

--- src/main/kotlin/functions/argHandlers.kt
+++ src/main/kotlin/functions/argHandlers.kt
@@ -1,11 +1,5 @@
 package functions
 
-fun Array<String>.ifArg(arg: String, block: () -> Unit) = if (arg in this) block() else Unit
-
-suspend fun Array<String>.ifArgCoroutine(arg: String, block: suspend () -> Unit) {
-	if (arg in this) block()
-}
-
 fun Array<String>.ifNoArg(arg: String, block: () -> Unit) = if (arg !in this) block() else Unit
 
 suspend fun Array<String>.ifNoArgCoroutine(arg: String, block: suspend () -> Unit) {
