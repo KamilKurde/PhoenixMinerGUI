e614c7dba326c7beacb1dca0edc246b784cd3506
Kamil Bąk <kamilbak113@gmail.com>
2021-10-10 17:36:31 +0200

    Fix share founding not updating UI

--- src/main/kotlin/Incrementable.kt
+++ src/main/kotlin/Incrementable.kt
@@ -1,11 +1,14 @@
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 // Class which value cannot be lowered, if new value is lower than previous that means error occured and value isn't taking pre-error value into consideration
 class Incrementable<T>(value: Int) : ReadWriteProperty<T, Int>
 {
-	private var current = value
-	private var old = 0
+	private var current by mutableStateOf(value)
+	private var old by mutableStateOf(0)
 	override fun getValue(thisRef: T, property: KProperty<*>) = current + old
 
 	override fun setValue(thisRef: T, property: KProperty<*>, value: Int)
