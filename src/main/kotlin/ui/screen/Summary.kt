52063116b1fbbedb7611fc20cf4f98c13e7775eb
Kamil Bąk <kamilbak113@gmail.com>
2021-10-31 07:58:16 +0100

    Added padding to "Miners" label

--- src/main/kotlin/ui/screen/Summary.kt
+++ src/main/kotlin/ui/screen/Summary.kt
@@ -24,37 +24,35 @@ import ui.table.MinerTable
 @ExperimentalFoundationApi
 @Composable
 fun Summary() = Column(modifier = Modifier.fillMaxSize().padding(8.dp), verticalArrangement = Arrangement.Top) {
-    Column(modifier = Modifier.fillMaxWidth().height(SPACERS_HEIGHT.dp), verticalArrangement = Arrangement.SpaceEvenly, horizontalAlignment = Alignment.CenterHorizontally) {
-        Text("GPUs", fontWeight = FontWeight.Bold)
-    }
-    GpuTable(Modifier.weight(1f, false))
-    Column(modifier = Modifier.fillMaxWidth().height(SPACERS_HEIGHT.dp), verticalArrangement = Arrangement.SpaceEvenly, horizontalAlignment = Alignment.CenterHorizontally) {
-        Text("Miners", fontWeight = FontWeight.Bold)
-    }
-    MinerTable(Modifier.weight(1f, false))
-    Spacer(Modifier.height(SPACERS_HEIGHT.dp))
-    Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically)
-    {
-        Text(VERSION)
-        // This doesn't work without full distributable, distributable gives exe location as its path, while running normally from IDEA program gives project location as its path
-        if (VERSION != "DEVELOPMENT")
-        {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Text("Launch PhoenixMiner GUI on system startup")
-                Spacer(Modifier.width(16.dp))
-                var enabled by remember { mutableStateOf(Shortcut.exist) }
-                Switch(
-                    enabled,
-                    {
-                        enabled = if (it) {
-                            Shortcut.create()
-                            true
-                        } else {
-                            Shortcut.delete()
-                            false
-                        }
-                    })
-            }
-        }
-    }
+	Column(modifier = Modifier.fillMaxWidth().height(SPACERS_HEIGHT.dp), verticalArrangement = Arrangement.SpaceEvenly, horizontalAlignment = Alignment.CenterHorizontally) {
+		Text("GPUs", fontWeight = FontWeight.Bold)
+	}
+	GpuTable(Modifier.weight(1f, false))
+	Column(modifier = Modifier.padding(top = SPACERS_HEIGHT.dp).fillMaxWidth().height(SPACERS_HEIGHT.dp), verticalArrangement = Arrangement.SpaceEvenly, horizontalAlignment = Alignment.CenterHorizontally) {
+		Text("Miners", fontWeight = FontWeight.Bold)
+	}
+	MinerTable(Modifier.weight(1f, false))
+	Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically)
+	{
+		Text(VERSION)
+		// This doesn't work without full distributable, distributable gives exe location as its path, while running from IDEA program gives project location as its path
+		if (VERSION != "DEVELOPMENT") {
+			Row(verticalAlignment = Alignment.CenterVertically) {
+				Text("Launch PhoenixMiner GUI on system startup")
+				Spacer(Modifier.width(16.dp))
+				var enabled by remember { mutableStateOf(Shortcut.exist) }
+				Switch(
+					enabled,
+					{
+						enabled = if (it) {
+							Shortcut.create()
+							true
+						} else {
+							Shortcut.delete()
+							false
+						}
+					})
+			}
+		}
+	}
 }
