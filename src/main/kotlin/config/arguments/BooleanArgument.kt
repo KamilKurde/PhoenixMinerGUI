fb389b29ef866aa5651a08e809689b95eb38003a
Kamil Bąk <kamilbak113@gmail.com>
2021-12-08 21:25:42 +0100

    Changed log parameter to be required

--- src/main/kotlin/config/arguments/BooleanArgument.kt
+++ src/main/kotlin/config/arguments/BooleanArgument.kt
@@ -2,7 +2,6 @@ package config.arguments
 
 import config.*
 
-enum class BooleanArgument(override val  description: String, override val  parameter: String, override val required: Boolean): CommandlineArgument
-{
-	Log("Selects the log file mode: disabled, no log file will be written; enabled, write log file", "log", false)
+enum class BooleanArgument(override val description: String, override val parameter: String, override val required: Boolean) : CommandlineArgument {
+	Log("Selects the log file mode: disabled, no log file will be written; enabled, write log file", "log", true)
 }
