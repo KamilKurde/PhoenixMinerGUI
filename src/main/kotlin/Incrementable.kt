5f88a96a5e9a1ac5ab18e2ed715150ecc2c3411f
Kamil Bąk <kamilbak113@gmail.com>
2021-12-13 17:45:24 +0100

    Fixed a typo

--- src/main/kotlin/Incrementable.kt
+++ src/main/kotlin/Incrementable.kt
@@ -2,7 +2,7 @@ import androidx.compose.runtime.*
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-// Class which value cannot be lowered, if new value is lower than previous that means error occured and value isn't taking pre-error value into consideration
+// Class which value cannot be lowered, if new value is lower than previous that means error occurred and value isn't taking pre-error value into consideration
 class Incrementable<T>(value: Int) : ReadWriteProperty<T, Int> {
 	private var current by mutableStateOf(value)
 	private var old by mutableStateOf(0)
