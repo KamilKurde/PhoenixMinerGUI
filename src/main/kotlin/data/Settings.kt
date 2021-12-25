4885f8dfed2d394389188d1897fbabbbb901a782
Kamil Bąk <kamilbak113@gmail.com>
2021-12-25 23:00:21 +0100

    Fixed miner not restarting on crash

--- src/main/kotlin/data/Settings.kt
+++ src/main/kotlin/data/Settings.kt
@@ -75,7 +75,7 @@ object Settings {
 	private val minersToStart = mutableListOf<Miner>()
 
 	fun startMiner(miner: Miner) {
-		if (miner !in minersToStart && miner !in activeMiners) {
+		if (miner !in minersToStart) {
 			miner.status = MinerStatus.Waiting
 			minersToStart.add(miner)
 		}
